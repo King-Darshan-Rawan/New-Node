bc97805d0e7d6d9c0b95ccb2eda6b4f617931151
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-07-09 13:44:34 +0530

    Maths to Arthmetic operations

---
+++ 01/math.js
@@ -0,0 +1,17 @@
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function sub(a, b) {
+//     return a - b;
+// }
+
+// module.exports = {
+//     addfn: add,
+//     subfn: sub
+// };
+
+
+exports.addfn = (a, b) => a + b;
+exports.subfn = (a, b) => a - b;
