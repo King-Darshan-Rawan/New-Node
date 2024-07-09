759199ab3f3ba08e210b539ca59f8825890c0e64
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-07-09 13:44:09 +0530

    Yet to started

--- 01/Hello.js
+++ 01/Hello.js
@@ -1,1 +1,5 @@
-console.log("new DB with JS")
+const math = require("./math");
+
+console.log("new DB with JS");
+console.log("math Value is:", math.addfn(2, 8));
+console.log("sub Value is:", math.subfn(10, 3));
